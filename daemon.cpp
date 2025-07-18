64a3182e622917d9dbee4acbed7f9bb99edb7b36
Scott Atchley <atchleyes@ornl.gov>
2025-07-18 09:28:46 -0400

    delete unsed daemoinze and signal code

--- daemon.cpp
+++ daemon.cpp
@@ -349,51 +349,6 @@ void print_values(const std::unordered_map<std::string, double> &values) {
 	}
 }
 
-// Daemonize the process
-void daemonize() {
-	pid_t pid = fork();
-	if (pid < 0) {
-		std::cerr << "Fork failed: " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
-	if (pid > 0) {
-		exit(0); // Parent exits
-	}
-
-	// TODO - set up ROCm context and start counters
-
-	// The following is needed to prevent the Slurm prolog script from killing the daemon
-
-	// Create new session
-	if (setsid() < 0) {
-		std::cerr << "Failed to create new session: " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
-
-#ifdef NDEBUG
-	// Redirect standard files to /dev/null
-	int fd = open("/dev/null", O_RDWR);
-	if (fd != -1) {
-		dup2(fd, STDIN_FILENO);
-		dup2(fd, STDOUT_FILENO);
-		dup2(fd, STDERR_FILENO);
-		if (fd > 2) close(fd);
-	} else {
-		std::cerr << "Failed to open /dev/null: " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
-
-	// Set umask
-	umask(0);
-
-	// Change working directory to root
-	if (chdir("/") < 0) {
-		std::cerr << "Failed to change directory to /: " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
-#endif
-}
-
 // Must be passed one argument, config-[01]
 int main(int argc, char *argv[]) {
 	// 1. Check for NO_OLCF_ROCPROF environment variable
@@ -487,28 +442,6 @@ int main(int argc, char *argv[]) {
 	std::cerr << "Opened file " << dirname << "/" << filename  << std::endl;
 	//output_file << "Opened file " << dirname << "/" << filename  << std::endl;
 
-#if 0
-	// Set read-write permissions for owner (rw-------)
-	if (chmod(("/var/log/" + filename).c_str(), S_IRUSR | S_IWUSR) != 0) {
-		std::cerr << "Failed to set permissions on /var/log/" << filename << ": " << std::strerror(errno) << std::endl;
-		output_file.close();
-		return 1;
-	}
-#endif
-
-#if 0
-	// Block waiting for SIGUSR1, SIGTERM, or SIGINT
-	sigset_t set;
-	sigemptyset(&set);
-	sigaddset(&set, SIGUSR1);
-	sigaddset(&set, SIGTERM);
-	sigaddset(&set, SIGINT);
-	sigprocmask(SIG_BLOCK, &set, nullptr);
-#endif
-
-	// Daemonize the process
-	//daemonize();
-
 	// Get our PID and write to a file in tmp
 	pid_t pid = getpid();
 	std::string pid_filename = std::string("/tmp/rocm-counter-daemon-pid-") + std::string(slurm_jobid);
@@ -571,24 +504,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	//output_file << "Exiting loop" << std::endl;
-
-#if 0
-	// Wait for signal
-	int signum;
-	if (sigwait(&set, &signum) != 0) {
-		std::cerr << "Failed to wait for signal: " << std::strerror(errno) << std::endl;
-		output_file.close();
-		return 1;
-	}
-
-	if (signum == SIGUSR1) {
-		// TODO
-		// Read counters and write to output_file
-		output_file << "Hello World!" << std::endl;
-		output_file.flush();
-	}
-	output_file.close();
-#endif
 
 	//std::cout << "end:\n";
 	for (auto collector : collectors) {
